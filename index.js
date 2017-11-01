0852ca7b7ae5fc05f870acce88472a06bf5883e3
Thomas Brüggemann <mail@thomasbrueggemann.com>
2017-11-01 20:11:37 +0100

    init restify

---
+++ index.js
@@ -0,0 +1,14 @@
+const restify = require("restify");
+
+// init server
+const server = restify.createServer();
+
+server.get("/hello/:name", (req, res, next) => {
+	res.send("hello " + req.params.name);
+	return next();
+});
+
+// listen to port 8080
+server.listen(8080, function() {
+	console.log("%s listening at %s", server.name, server.url);
+});
